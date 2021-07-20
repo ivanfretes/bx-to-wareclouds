d1f78f936075177976585cc90d0d0634159bb933
Iván Fretes <vanfretes@gmail.com>
2021-07-20 12:57:21 -0400

    connect the bucket gcs

---
+++ app/src/services/CloudStorageSrv.js
@@ -0,0 +1,17 @@
+const {Storage} = require('@google-cloud/storage');
+
+const CloudStorageSrv = {};
+
+CloudStorageSrv.uploadFile = () => {
+   const storage = new Storage({keyFilename: 'key.json'});
+   const bucketName = 'warecloud_bucked_01';
+   
+   async function createBucket() {
+      // Creates the new bucket
+      await storage.createBucket(bucketName);
+      console.log(`Bucket ${bucketName} created.`);
+   }
+}
+
+
+module.exports = CloudStorageSrv;
