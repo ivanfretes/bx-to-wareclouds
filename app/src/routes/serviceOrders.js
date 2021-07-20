598aa4c90f853a253dd038fc3b228c6b051a5349
Iván Fretes <vanfretes@gmail.com>
2021-07-20 12:30:47 -0400

    add : order srv (fix)

--- app/src/routes/serviceOrders.js
+++ app/src/routes/serviceOrders.js
@@ -2,103 +2,43 @@ const app = express();
 const router = express.Router();
 const { Op } = require("sequelize");
 const axios = require('axios');
-
+const orderSrv = require('../services/OrderSrv');
 
 // GENERATE OS for order valid
 router.get('/valid', async (req, res) => {
-
    const orders = await Order.findAll({
       where : {
          id_status_order : 6
-      }
+      },
+      include : { Ecommerce }
    });
 
    orders.forEach((order, index) => {
-      const body = {
-         "printFormatCode": 4,
-         "orderNumber": "9999152408692",
-         "references": [],
-         "serviceType": "EX",
-         "productType": "P",
-         "productCategory": "PAQU",
-         "currency": "CLP",
-         "shipmentCost": 0,
-         "extendedClaim": false,
-         "companyId": 2000,
-         "userName": "USRWCLOUD",
-         "comments": "Comentario de Prueba",
-         "pickup": {
-            "location": {
-               "stateId": 15,
-               "districtId": "AGN",
-               "address": "Calle 2,2",
-               "name": "CESAR"
-            },
-            "contact": {
-               "fullname": "Jorge Munoz",
-               "phone": "54544522"
-            }
-         },
-         "dropoff": {
-            "contact": {
-               "fullname": "Juan Perez",
-               "phone": "+56123456789"
-            },
-            "location": {
-               "stateId": 9,
-               "districtId": "VIC",
-               "address": "Direccion X",
-               "name": "Direccion de Entrega"
-            }
-         },
-         "packages": [
-            {
-               "weightUnit": "KG",
-               "lengthUnit": "CM",
-               "weight": 1,
-               "length": 1,
-               "width": 1,
-               "height": 1,
-               "quantity": 1,
-               "extras": [
-                  {
-                     "name": "name_of_package",
-                     "value": "Paquete X"
-                  }
-               ]
-            }
-         ],
-         "notificationContacts": [
-            {
-               "contactType": "R",
-               "contactChannel": 2,
-               "value": "ifretes@gux.cl",
-               "events": [
-                  {
-                     "code": 5
-                  },
-                  {
-                     "code": 21
-                  }
-               ]
-            }
-         ]
-         };
-         
-         const response = await axios.post(`${process.env.BLUE_EXPRESS_API}/bx-emission/v1`, 
-            JSON.stringify(body) , {
-            headers : {
-               'Accept': 'application/json', 
-               'Content-Type': 'application/json', 
-               'BX-TOKEN': process.env.BX_TOKEN, 
-               'BX-USERCODE': process.env.BX_USERCODE, 
-               'BX-CLIENT_ACCOUNT': process.env.BX_CLIENT_ACCOUNT
-            },
-
-         });
+      orderSrv.generateOS(order);
    });
 
-   return res.json(order);
+   return res.render('Se generaron las ordenes');
+})
+
+// GENERATE OS for order selected
+router.get('/selected', async (req, res) => {
+   const { selected } = res.body
+
+   // validacion
+
+   const orders = await Order.findAll({
+      where : {
+         id_status_order : 6,
+         id : selected
+      },
+      include : { Ecommerce }
+   });
+
+   orders.forEach((order, index) => {
+      orderSrv.generateOS(order);
+   });
+
+   return res.render('Se generaron las ordenes');
 })
 
 
---
+++ app/src/services/OrderSrv.js
@@ -0,0 +1,100 @@
+const OrderSrv = {};
+
+OrderSrv.generateOS = (order) => {
+   let orderNumberStr = order.order_code + order.Ecommerce.tax_registry_number; 
+   if (orderNumberStr.lenght > 20)
+      orderNumberStr = orderNumberStr.substring(orderNumberStr.lenght - 20)
+  
+
+   const body = {
+      "printFormatCode": 4,
+      "orderNumber": orderNumber,
+      "references": [],
+      "serviceType": "EX",
+      "productType": "P",
+      "productCategory": "PAQU",
+      "currency": "CLP",
+      "shipmentCost": 0,
+      "extendedClaim": false,
+      "companyId": 2000,
+      "userName": "USRWCLOUD",
+      "comments": "Comentario de Prueba",
+      "pickup": {
+         "location": {
+            "stateId": 15,
+            "districtId": "AGN",
+            "address": "Calle 2,2",
+            "name": "CESAR"
+         },
+         "contact": {
+            "fullname": "Jorge Munoz",
+            "phone": "54544522"
+         }
+      },
+      "dropoff": {
+         "contact": {
+            "fullname": "Juan Perez",
+            "phone": "+56123456789"
+         },
+         "location": {
+            "stateId": 9,
+            "districtId": "VIC",
+            "address": "Direccion X",
+            "name": "Direccion de Entrega"
+         }
+      },
+      "packages": [
+         {
+            "weightUnit": "KG",
+            "lengthUnit": "CM",
+            "weight": 1,
+            "length": 1,
+            "width": 1,
+            "height": 1,
+            "quantity": 1,
+            "extras": [
+               {
+                  "name": "name_of_package",
+                  "value": "Paquete X"
+               }
+            ]
+         }
+      ],
+      "notificationContacts": [
+         {
+            "contactType": "R",
+            "contactChannel": 2,
+            "value": "ifretes@gux.cl",
+            "events": [
+               {
+                  "code": 5
+               },
+               {
+                  "code": 21
+               }
+            ]
+         }
+      ]
+      };
+      
+      const response = await axios.post(`${process.env.BLUE_EXPRESS_API}/bx-emission/v1`, 
+         JSON.stringify(body) , {
+         headers : {
+            'Accept': 'application/json', 
+            'Content-Type': 'application/json', 
+            'BX-TOKEN': process.env.BX_TOKEN, 
+            'BX-USERCODE': process.env.BX_USERCODE, 
+            'BX-CLIENT_ACCOUNT': process.env.BX_CLIENT_ACCOUNT
+         },
+      });
+
+      if (response.status == 200){
+         const { tracking, labels } = orderNumber.data.data;
+         
+         
+      } else {
+         console.log('Error')
+      }
+}
+
+module.exports = OrderSrv;
