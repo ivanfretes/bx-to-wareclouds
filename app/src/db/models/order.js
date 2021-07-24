49a5d67c4b201e11320976706f6f37d38e24f3ca
Iván Fretes <vanfretes@gmail.com>
2021-07-23 20:02:32 -0400

    migration schema

--- app/src/db/models/order.js
+++ app/src/db/models/order.js
@@ -14,7 +14,48 @@ module.exports = (sequelize, DataTypes) => {
     }
   };
   Order.init({
-    id_order: DataTypes.INTEGER
+    id_order: {
+      type: DataTypes.INTEGER({ length : 11 }),
+   order_code : DataTypes.STRING(20),
+   id_ecommerce : {
+      type : DataTypes.STRING(45),
+   },
+   id_warecloud : {
+      type : DataTypes.STRING(45),
+   },
+   id_route : {
+      type : DataTypes.INTEGER(11)
+   },
+   id_status_order : {
+      type : DataTypes.INTEGER(11)
+   },
+   first_name : {
+      type : DataTypes.STRING(45),
+   },
+   last_name : {
+      type : DataTypes.STRING(45),
+   },
+   email : {
+      type : DataTypes.STRING(45),
+   },
+   address : {
+      type : DataTypes.STRING(200),
+   },
+   address_detail : {
+      type : DataTypes.STRING(500),
+   },
+   address_extra_info : {
+      type : DataTypes.STRING(500),
+   },
+   id_city : {
+      type : DataTypes.INTEGER({length : 11})
+   },
+   id_country : {
+      type : DataTypes.INTEGER({length : 11})
+   },
+   phone : {
+      type : DataTypes.STRING(45),
+   }
   }, {
     sequelize,
     modelName: 'Order',
